0b74383a55964ae372795d330db59761b4010e9b
Gustavo Tenório <gustavohst@msn.com>
2020-11-01 14:18:01 -0300

    Create project without render

--- web/src/components/CreateProject/CreateProject.jsx
+++ web/src/components/CreateProject/CreateProject.jsx
@@ -1,16 +1,43 @@
-import React from 'react';
-import './CreateProject.css'
+import React,{ useState } from 'react';
+import './CreateProject.css';
 
-import Button from '../Button/Button'
-import Label from '../Label/Label'
-import TextField from '../TextField/TextField'
+import Button from '../Button/Button';
+import Label from '../Label/Label';
+import TextField from '../TextField/TextField';
+import api from '../../services/api';
 
-function CreateProject() {
+function CreateProject(props) {
+   const {
+      userId = 1,
+   } = props;
+
+   const [newProjectName, setNewProjectName] = useState();
+
+   const handleCreateProject = async (projectName) => {
+      const payload = {
+            "user_id": userId,
+            "name": projectName,
+      };
+      await api.post(`projects`, payload).then(response => {
+         console.log(response.data);
+      });
+   }
+
    return (
       <div className="createProjectContainer">
-         <Label type="bigTitle" text="Create a new project" />
-         <TextField placeholder="Project name" />
-         <Button layout="largeButton" label="Create Project" />
+         <Label 
+            type="bigTitle" 
+            text="Create a new project"      
+         />
+         <TextField 
+            placeholder="Project name" 
+            onBlur={event => setNewProjectName(event.target.value)}  
+         />
+         <Button
+            layout="largeButton"
+            label="Create Project"
+            onClick={() => handleCreateProject(newProjectName)}
+         />
       </div>
    );
 }
